2f6e83acaf95395fd65f4167a4be2cc1b88e1c15
carter-willey <cjwilley23@gmail.com>
2021-06-16 12:45:22 -0500

    display welcome and rules now prints to terminal on start

--- ai.py
+++ ai.py
@@ -4,6 +4,6 @@ import random
 class AI(Player):
     def __init__(self):
         super().__init__()
-        
+
     def choose_gesture(self):
         self.chosen_gesture = self.gestures[random.randint(0,4)]
--- game.py
+++ game.py
@@ -1,18 +1,34 @@
 from human import Human
 from ai import AI
+import time
 
 class Game:
     def __init__(self):
-        self.player_one = Human()
+        #self.player_one = Human()
         self.player_two = None
-        self.player_one.display_gestures()
+        self.run_game()
 
     def display_welcome(self):
         print("Hello and welcome to Rock, Paper, Scissors, Lizard, Spock!")
+    def display_rules(self):
+        print("The rules are similar to your standard game of rock paper scissors. However, we have two added two more options: lizard and Spock")
+        time.sleep(.75)
+        print("Your standard rock, paper, scissors rules apply with the following additional rules:\n"
+              "Rock crushes Lizard\n"
+              "Lizard poisons Spock\n"
+              "Spock smashes Scissors\n"
+              "Scissors decapitates Lizard\n"
+              "Lizard eats Paper\n"
+              "Paper disproves Spock\n"
+              "Spock vaporizes Rock\n"              )
+
+
 
     def run_game(self):
         # Intro
-        #Display welcome
+        self.display_welcome()
+        time.sleep(.75)
+        self.display_rules()
         #Instructions
         #Pick game mode - single player or multiplayer
 
