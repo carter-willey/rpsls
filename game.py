2925d666d25873552eb78f89f3d50fb18ff67a3f
carter-willey <cjwilley23@gmail.com>
2021-06-17 14:09:19 -0500

    simplified logic for input verification/testing

--- game.py
+++ game.py
@@ -50,6 +50,10 @@ class Game:
 
         print("Would you like to play again?")
         play_again = input("Enter '1' for yes or '2' for no: ")
+        while play_again != "1" and play_again != "2":
+            print("Please enter either '1' or '2'")
+            play_again = input("Enter '1' for yes or '2' for no: ")
+
         if play_again == "1":
             self.run_game()
         else:
@@ -58,7 +62,7 @@ class Game:
 
 
     def check_gestures(self):
-        if self.player_one.chosen_gesture == self.player_two.chosen_gesture:
+        if self.player_one.chosen_gesture.name == self.player_two.chosen_gesture.name:
             print(f"Both players picked {self.player_one.chosen_gesture}. This round is a draw")
         elif (self.player_one.chosen_gesture.name == self.player_two.chosen_gesture.gets_beat_by[0]) or (self.player_one.chosen_gesture.name == self.player_two.chosen_gesture.gets_beat_by[1]):
             self.player_one.score += 1
@@ -79,17 +83,12 @@ class Game:
     def choose_game_mode(self):
         print("What game mode would you like to play?")
         game_mode = input("Please enter '1' for Single player or '2' for Two player: ")
-        if game_mode != "1" and game_mode != "2":
-            while game_mode != "1" and game_mode != "2":
-                print("Please enter either '1' or '2'")
-                game_mode = input("Please enter '1' for Single player or '2' for Two player: ")
-            game_mode = int(game_mode)
-            self.game_state = True
-        else:
-            game_mode = int(game_mode)
-            self.game_state = True
+        while game_mode != "1" and game_mode != "2":
+            print("Please enter either '1' or '2'")
+            game_mode = input("Please enter '1' for Single player or '2' for Two player: ")
+        self.game_state = True
 
-        if game_mode == 1:
+        if game_mode == "1":
             self.player_one.get_name("One")
             self.player_two = AI()
         else:
